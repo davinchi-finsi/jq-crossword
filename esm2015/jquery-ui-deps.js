ad0afb6a7e787b872f2d671346a1ad2088c1f1aa
Javier Pérez Ruiz <javier.perez@davinchi.es>
2018-03-09 13:03:24 +0100

    fix: Added jquery-ui-deps.js to vcs

---
+++ esm2015/jquery-ui-deps.js
@@ -0,0 +1,8 @@
+/**
+ * Required jquery ui dependencies
+ */
+import "jquery-ui/ui/version";
+import "jquery-ui/ui/plugin";
+import "jquery-ui/ui/data";
+import "jquery-ui/ui/widget";
+import "jquery-ui/ui/keycode";
